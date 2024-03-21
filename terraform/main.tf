26a4f6757534696e14a7acc058240dd44a333cc0
subrockmann <newinmunich@aol.com>
2024-03-21 21:55:43 +0100

    feat: build bigquery tables with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,41 +8,77 @@ terraform {
 }
 
 provider "google" {
-# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-#  credentials = 
-  project = "farm-screener"
-  region  = "europe-west3"
+  # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  #  credentials = 
+  project = var.project
+  region  = var.region
 }
 
 
-
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "farm-screener-tf"
-  location      = "europe-west3"
+resource "google_storage_bucket" "raw-data-bucket" {
+  name          = var.gcs_bucket_name_raw
+  location      = var.region
+  force_destroy = false # prevents deletion of the bucket if it is not empty
 
   # Optional, but recommended settings:
-  storage_class = "STANDARD"
+  storage_class               = var.gcs_storage_class
   uniform_bucket_level_access = true
 
   versioning {
-    enabled     = true
+    enabled = true
   }
 
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      age = 30  // days
-    }
-  }
-
-  force_destroy = true
+  #   lifecycle_rule {
+  #     action {
+  #       type = "Delete"
+  #     }
+  #     condition {
+  #       age = 0 #30  // days
+  #     }
+  #   }
 }
 
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "farm_screener_tf"
-  project    = "farm-screener"
-  location   = "EU"
+resource "google_storage_bucket" "clean-data-bucket" {
+  name          = var.gcs_bucket_name_clean
+  location      = var.region
+  force_destroy = false # prevents deletion of the bucket if it is not empty
+
+  # Optional, but recommended settings:
+  storage_class               = var.gcs_storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id = var.bq_dataset_name
+  project    = var.project
+  location   = var.location
+}
+
+resource "google_bigquery_table" "offers" {
+  dataset_id          = google_bigquery_dataset.default.dataset_id
+  table_id            = var.bq_dataset_table_name_offers
+  deletion_protection = true
+
+  #   time_partitioning {
+  #     type = "DAY"
+  #   }
+
+  labels = {
+    env = "default"
+  }
+
+}
+resource "google_bigquery_table" "farms" {
+  dataset_id          = google_bigquery_dataset.default.dataset_id
+  table_id            = var.bq_dataset_table_name_farms
+  deletion_protection = true
+
+  labels = {
+    env = "default"
+  }
 }
